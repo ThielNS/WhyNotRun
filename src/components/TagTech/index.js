1d372225e92d1add2b54e8fbf08d36cd2bdaa1af
Senai Informática <thiel_13@hotmail.com>
2017-11-23 14:15:30 -0300

    create searchForm componnet and create resultForm

--- src/components/TagTech/index.js
+++ src/components/TagTech/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './tagstech.css'
 
-const renderTechies = techies => {
-  return techies.map((techie, index) => (
-    <span className="tag" key={index}>{techie.Name}</span>
-  ))
-}
+const renderTechies = (techies) => {
+  return techies.slice(0,2).map((techie, index) => (
+    <span className="tag" key={index}>{techie.name}</span>
+  ));
+};
 
 const TagTech = ({ tags }) => {
+
+  const lengthTags = tags.length;
+
   return (
     <div className="tags-tech">
       {renderTechies(tags)}
+      {lengthTags > 2 ? (<span className="tag">{`+${lengthTags - 2}`}</span>) : null}
     </div>
   );
-}
+};
 
 export default TagTech;
 
