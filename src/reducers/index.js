aba6d7b108f7a98455b06019362619fee435a4d1
Juliana Cerqueira de Abreu <juliana.cerqueira55@gmail.com>
2017-11-24 10:30:36 -0200

    changes in reducers - conflict

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,7 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
-import filterTechReducer from './filterTechReducer';
-
-const reducerFilter = combineReducers ({
-    technologies: filterTechReducer
-})
-export default reducerFilter;
-=======
 import publicationsReducer from './PublicationsReducer';
 import loginReducer from "./LoginReducer";
 
 const reducerApp = combineReducers({publicationsReducer, loginReducer});
 
 export default reducerApp
->>>>>>> origin/navbar
