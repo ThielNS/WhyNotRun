3d6f988a95fd7b119259a471221ee82c5780c624
Juliana Cerqueira de Abreu <juliana.cerqueira55@gmail.com>
2017-11-30 17:09:14 -0200

    create action for register

--- src/actions/registerAction.js
+++ src/actions/registerAction.js
@@ -1,1 +1,12 @@
 import { LOGIN_USER, VALID_LOGIN } from "./constants";
+
+
+export const RegisterUser = (name, email, profession, password, confirmPassword) => {
+   const bodyParams = {
+       name:name,
+       email:email, 
+       profession:profession,
+       password:password,
+       confirmPassword:confirmPassword
+   }
+}
