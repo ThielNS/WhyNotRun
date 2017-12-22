2376ab9921d3fb60084a625f7d6a212e2e7b063e
Senai Informática <thiel_13@hotmail.com>
2017-12-22 10:19:07 -0300

    bug fixed #7 - Fix bug in the bar status agree and disagree, and add class row -space-between in the bar

--- src/components/BarStatus/index.js
+++ src/components/BarStatus/index.js
@@ -5,11 +5,16 @@ import gif from './spark-v3.gif';
 const BarStatus = ({ agree, disagree }) => {
 
   const total = agree + disagree;
-  const percentAgree = (agree / total) * 100;
-  const percentDisagree = (disagree / total) * 100;
+  let percentAgree = (agree / total) * 100;
+  let percentDisagree = (disagree / total) * 100;
+
+  if (!percentAgree)
+    percentAgree = 0;
+  if (!percentDisagree)
+    percentDisagree = 0;
 
   return (
-    <div className="bar-status">
+    <div className="bar-status row -space-between">
       <div className="bar -agree" style={{width: `${percentAgree}%`}}/>
       <div className="spark-bar">
         <img src={gif} alt=""/>
